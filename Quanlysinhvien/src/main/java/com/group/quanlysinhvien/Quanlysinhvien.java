66a5ca78506f865b1da54d56f96b3fa38eaafaec
Thịnh Trần <thinhtran3803@gmail.com>
2023-02-28 20:53:38 +0700

    Init project

---
+++ Quanlysinhvien/src/main/java/com/group/quanlysinhvien/Quanlysinhvien.java
@@ -0,0 +1,14 @@
+
+
+package com.group.quanlysinhvien;
+
+/**
+ *
+ * @author ThinhTran
+ */
+public class Quanlysinhvien {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

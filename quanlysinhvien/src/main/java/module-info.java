ac48f294b675d0a3faebbdf37e1eb3f3197bc7a3
Thịnh Trần <thinhtran3803@gmail.com>
2023-03-29 22:40:10 +0700

    add  requires for sql

--- quanlysinhvien/src/main/java/module-info.java
+++ quanlysinhvien/src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module com {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires transitive javafx.graphics;
 
     opens com to javafx.fxml;
+
     exports com;
 }

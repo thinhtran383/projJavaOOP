4dc33c3399e226da400067c8b92861493600428d
Thịnh Trần <thinhtran3803@gmail.com>
2023-03-31 10:57:04 +0700

    add requires

--- quanlysinhvien/src/main/java/module-info.java
+++ quanlysinhvien/src/main/java/module-info.java
@@ -3,6 +3,10 @@ module com {
     requires javafx.fxml;
     requires java.sql;
     requires transitive javafx.graphics;
+    requires java.desktop;
+
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
 
     opens com to javafx.fxml;
 

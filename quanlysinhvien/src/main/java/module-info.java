0ac00604a03f075b95b3c6f4637c1115d18eac1f
Thịnh Trần <thinhtran3803@gmail.com>
2023-04-03 11:24:51 +0700

    them phu thuoc

--- quanlysinhvien/src/main/java/module-info.java
+++ quanlysinhvien/src/main/java/module-info.java
@@ -4,14 +4,18 @@ module com {
     requires java.sql;
     requires transitive javafx.graphics;
     requires java.desktop;
+    requires java.base;
 
-    // requires org.apache.poi.poi;
-    // requires org.apache.poi.ooxml;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
 
+    // requires org.apache.logging.log4j.core;
+    // requires org.apache.logging.log4j.slf4j;
     opens com to javafx.fxml;
 
     exports com;
     exports com.Controller;
+    exports com.Models;
 
     opens com.Controller to javafx.fxml;
 }

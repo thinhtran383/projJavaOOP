db3e15af80ff3272d7e8f85fc8ce692667812799
Thịnh Trần <thinhtran3803@gmail.com>
2023-04-12 01:29:05 +0700

    hoan thien chuc nang cua 2 nut

--- quanlysinhvien/src/main/java/com/Controller/GradesManagementController.java
+++ quanlysinhvien/src/main/java/com/Controller/GradesManagementController.java
@@ -1,11 +1,16 @@
 package com.Controller;
 
+import com.Helper.AlertHelper;
 import com.Helper.DataManager;
 import com.Models.Grade;
+import com.utils.ExecuteQuery;
+import com.utils.ExportToExcel;
 
 import javafx.beans.Observable;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -52,6 +57,16 @@ public class GradesManagementController {
         showOnTable();
     }
 
+    private void clear() {
+        txtStudentId.setText("");
+        txtStudentName.setText("");
+        txtSubjectId.setText("");
+        txtSubjectName.setText("");
+        txtAttendance.setText("");
+        txtMidterm.setText("");
+        txtFinal.setText("");
+    }
+
     private void showOnTable() {
         studentIdColumn.setCellValueFactory(new PropertyValueFactory<>("studentId"));
         studentNameColumn.setCellValueFactory(new PropertyValueFactory<>("studentName"));
@@ -72,5 +87,37 @@ public class GradesManagementController {
         txtAttendance.setText(String.valueOf(grade.getAttendanceGrade()));
         txtMidterm.setText(String.valueOf(grade.getMidTermGrade()));
         txtFinal.setText(String.valueOf(grade.getFinalGrade()));
+    }
+
+    public void onClickUpdate(ActionEvent actionEvent) {
+        String studentId = txtStudentId.getText();
+        String subjectId = txtSubjectId.getText();
+        float attendanceGrade = Float.parseFloat(txtAttendance.getText());
+        float midTermGrade = Float.parseFloat(txtMidterm.getText());
+        float finalGrade = Float.parseFloat(txtFinal.getText());
+
+        ExecuteQuery query = new ExecuteQuery("UPDATE grades SET attendance_grade ='" + attendanceGrade
+                + "', midterm_grade ='" + midTermGrade + "', final_grade ='" + finalGrade + "' WHERE student_id ='"
+                + studentId + "' AND course_id ='" + subjectId + "'");
+        query.executeUpdate();
+
+        Grade grade = tableGrades.getSelectionModel().getSelectedItem();
+
+        grade.setAttendanceGrade(attendanceGrade);
+        grade.setMidTermGrade(midTermGrade);
+        grade.setFinalGrade(finalGrade);
+
+        tableGrades.refresh();
+
+        AlertHelper.showAlert(AlertType.INFORMATION, "Thông báo", null, "Cập nhật thành công!");
+        clear();
+    }
+
+    public void onClickExport(ActionEvent actionEvent) {
+
+        if (AlertHelper.showConfirmation("Bạn có muốn xuất dữ liệu ra excel không?")) {
+            ExportToExcel.exportToExcel(tableGrades, "students.xlsx");
+            AlertHelper.showAlert(AlertType.INFORMATION, "Thông báo", null, "Xuất dữ liệu thành công!");
+        }
     }
 }
